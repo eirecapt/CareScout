ad6691c5e6c27f7974877f82838434c0a842fd4b
Michael Pangburn <michaelpangburn@comcast.net>
2018-11-03 13:55:25 -0700

    Baby steps

--- CareScout WatchKit Extension/InterfaceController.swift
+++ CareScout WatchKit Extension/InterfaceController.swift
@@ -7,10 +7,15 @@
 //
 
 import WatchKit
+import NightscoutKit
 import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var bloodGlucoseLabel: WKInterfaceLabel!
+
+    var manager: NightscoutDataManager?
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -21,6 +26,19 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+
+        let url = URL(string: "https://cgmpangburn.herokuapp.com")!
+        NightscoutDownloaderCredentials.validate(url: url) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let credentials):
+                self.manager = NightscoutDataManager(downloaderCredentials: credentials, dataStore: .allDataStore())
+                self.update()
+            case .failure(let error):
+                // TODO: handle error
+                break
+            }
+        }
     }
     
     override func didDeactivate() {
@@ -28,4 +46,36 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    func update() {
+        updateStatus(then: updateGlucose)
+    }
+
+    func updateStatus(then next: @escaping () -> Void) {
+        manager?.downloader.fetchStatus { _ in next() }
+    }
+
+    func updateGlucose() {
+        manager?.downloader.fetchMostRecentEntries(count: 1) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let entries):
+                guard let mostRecent = entries.first else {
+                    return // TODO: requested one but got none!
+                }
+                let unit = self.manager?.dataStore.fetchedStatus?.settings.bloodGlucoseUnits ?? .milligramsPerDeciliter
+
+                let displayString: String
+                switch mostRecent.converted(to: unit).source {
+                case .meter:
+                    displayString = String(describing: mostRecent.glucoseValue.valueString)
+                case .sensor(trend: let trend):
+                    displayString = "\(mostRecent.glucoseValue.valueString)\(trend)"
+                }
+                self.bloodGlucoseLabel.setText(displayString)
+            case .failure(let error):
+                // TODO: handle error
+                break
+            }
+        }
+    }
 }
