a392365a07e16f23e7bc129df33d17fc3a0974c1
Michael Pangburn <michaelpangburn@comcast.net>
2018-11-03 11:05:34 -0700

    Initial Commit

---
+++ CareScout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CareScout
+//
+//  Created by Michael Pangburn on 11/3/18.
+//  Copyright © 2018 Michael Pangburn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
